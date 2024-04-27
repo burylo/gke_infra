73375bbca1b41b712076cc3b7bf5c506f0f6c626
Yurii Burylo <y.burylo@gmail.com>
2024-04-27 11:19:22 +0000

    Ifrastructure

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "GOOGLE_REGION" {
+  type        = string
+  description = "Google cluster Region"
+}
+
+variable "GOOGLE_PROJECT" {
+  type        = string
+  description = "Google work project"
+}
